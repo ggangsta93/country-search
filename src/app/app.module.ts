0ef4aebba4e54cd42961c104eb9db2870dfaa633
Javier Arias <parias@heinsohn.com.co>
2022-10-03 22:12:02 -0500

    Se estructura proyectos creando los componentes y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ByCapitalComponent } from './country/pages/by-capital/by-capital.component';
+import { ByCountryComponent } from './country/pages/by-country/by-country.component';
+import { ByRegionComponent } from './country/pages/by-region/by-region.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
+import { CountryModule } from './country/country.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountryModule,
+    SharedModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/country/pages/see-country/see-country.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-country',
+  templateUrl: './see-country.component.html'
+})
+export class SeeCountryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
