fd6b586079b6f1a6d7874b32a383fddf2032b961
Javier Arias <parias@heinsohn.com.co>
2022-10-09 21:37:37 -0500

    Se ajusta componente by-country y relacionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ByCapitalComponent } from './country/pages/by-capital/by-capital.component';
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CountryModule,
     SharedModule
   
--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ByCountryComponent implements OnInit {
 
-  constructor() { }
+  public termino: string = "";
+  public esError = false;
+  public countries: Country[] = [];
+
+  constructor(private countryService: CountryService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino:string) {
+    this.esError = false;
+    this.termino = termino;
+    console.log(this.termino);
+    this.countryService.buscarPais(this.termino).subscribe(paises => {
+      this.countries = paises;
+    }, (err) => {
+      console.log('error');
+      this.esError = true;
+      this.countries = [];
+    });
   }
 
 }
