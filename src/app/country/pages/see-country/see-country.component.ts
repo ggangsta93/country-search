da5f0ef851bae317c20a46cceec690a1fa67374c
Javier Arias <parias@heinsohn.com.co>
2022-10-10 22:21:10 -0500

    Se utiliza operador switchMap de rxjs para obtener el id y llamar al observable de getCountryByAlpha

--- src/app/country/pages/see-country/see-country.component.ts
+++ src/app/country/pages/see-country/see-country.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-see-country',
@@ -6,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeeCountryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.pipe(
+      switchMap(({ id }) => this.countryService.getCountryByAlpha(id))
+    ).subscribe(resp => {
+      console.log(resp);
+    })
+
+    /*
+    this.activateRoute.params.subscribe(
+      ({ id }) => {
+        console.log(id);
+
+        this.countryService.getCountryByAlpha(id).subscribe(country => {
+          console.log(country);
+        });
+
+      }
+    )
+    */
   }
 
 }
